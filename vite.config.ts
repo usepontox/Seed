3ad43c4ec40fbe787df820bde8658c6d05eae26a
usepontox <usepontox@gmail.com>
2025-11-27 23:25:37 -0300

    Fix: Configure Vite to copy .htaccess to dist

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 }));
